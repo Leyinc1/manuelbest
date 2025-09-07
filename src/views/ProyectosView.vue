294e7836d015b62d0c01bee68238f1967231cfb6
Leyinc1 <manuelipenza@gmail.com>
2025-09-07 04:19:52 -0500

    Pagina 6.0.0

--- src/views/ProyectosView.vue
+++ src/views/ProyectosView.vue
@@ -2,5 +2,13 @@
   <section id="proyectos" class="view-card content-section active">
     <h2>Proyectos Destacados</h2>
     <p>Pagina Web Igd: <a href="https://igdgroupsac.com" target="_blank">IGD GROUP</a></p>
+    <p>
+      Proyecto de gestión hotelería (Python):
+      <a href="https://github.com/NAACH21/GEST_HOTEL_V2" target="_blank">Gestión Hotel V2</a>
+    </p>
+    <p>
+      Proyecto de gestión hotelería (Java):
+      <a href="https://github.com/Leyinc1/Gestion-Hotel" target="_blank">Gestión Hotel Java</a>
+    </p>
   </section>
 </template>
