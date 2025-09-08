730db1cc94d62bdbcdf997f6ed9809bd15bfd015
Manuel Ipenza <manuelipenza>
2025-09-08 09:56:16 -0500

    Calendario v1.0.0

--- src/views/CalendarioView.vue
+++ src/views/CalendarioView.vue
@@ -1,6 +1,60 @@
 <template>
-  <div>
-    <h1>Calendario</h1>
-    <p>Proximamente...</p>
+  <div class="calendar-view">
+    <FullCalendar :options="calendarOptions" />
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import FullCalendar from '@fullcalendar/vue3';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+  },
+  editable: true,
+  selectable: true,
+  select: handleDateSelect,
+  eventClick: handleEventClick,
+  events: [
+    { title: 'Meeting', start: new Date() }
+  ]
+});
+
+function handleDateSelect(selectInfo) {
+  let title = prompt('Please enter a new title for your event');
+  let calendarApi = selectInfo.view.calendar;
+
+  calendarApi.unselect(); // clear date selection
+
+  if (title) {
+    calendarApi.addEvent({
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay
+    });
+  }
+}
+
+function handleEventClick(clickInfo) {
+  if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+    clickInfo.event.remove();
+  }
+}
+
+</script>
+
+<style scoped>
+.calendar-view {
+  padding: 40px;
+  flex-grow: 1;
+}
+</style>
